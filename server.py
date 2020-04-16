b41c93dc7ec9ab4c6f873a6d6f3c4dfab4c74a6a
songjein <jeinsong100@gmail.com>
2020-04-16 14:44:16 +0000

    [ADD] text summarization

--- server.py
+++ server.py
@@ -1,18 +1,33 @@
 import time
 
 import spacy
+import pytextrank
 
 from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 
+
 # NER
 nlp = spacy.load('en_core_web_sm')
 target_labels = set(['PERSON', 'ORG', 'PRODUCT', 'PERCENT', 'MONEY', 'QUANTITY'])
 
 # TEXT-SUM
-# [TODO]
+tr = pytextrank.TextRank()
+nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
+
+
+@app.route('/sum', methods=['POST'])
+def sum_():
+    text = request.form.get('text')
+
+    doc = nlp(text)
+    sents = []
+    for sent in doc._.textrank.summary(limit_phrases=15, limit_sentences=3):
+        sents.append(str(sent))
+    sum_text = ' '.join(sents)
+    return jsonify({'status': 200, 'sum_text': sum_text })
 
 
 @app.route('/ner', methods=['POST'])
